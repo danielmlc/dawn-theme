a2b662dcb1bf9df4ab3d318655ff8186768f8595
danielmlc <danielmlc@126.com>
2019-06-19 15:37:34 +0800

    更换打包器

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const { series, src, dest } = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cssmin = require('gulp-cssmin');
+
+function compile() {
+  return src('./src/*.scss')
+    .pipe(sass.sync())
+    .pipe(autoprefixer({
+      cascade: false
+    }))
+    .pipe(cssmin())
+    .pipe(dest('./lib'));
+}
+
+
+exports.build = series(compile);
+
